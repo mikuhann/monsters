ab0a6593c5df93c3b13bf36291173f9238737135
Mikuhann <ng.kano@gmail.com>
2019-09-09 13:13:47 +0300

    Updates

---
+++ precache-manifest.c6586c2d748f9ec02b231101478af6a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999ae7163119e3a15e5cade0d7b20167",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "baa950dda5cf8e71147f",
+    "url": "/monsters/static/css/main.e0e44dd5.chunk.css"
+  },
+  {
+    "revision": "7aab9ae200bad22a3210",
+    "url": "/monsters/static/js/2.9ce0fbe2.chunk.js"
+  },
+  {
+    "revision": "baa950dda5cf8e71147f",
+    "url": "/monsters/static/js/main.ea9c8f5d.chunk.js"
+  },
+  {
+    "revision": "98e095b3e406d4e12025",
+    "url": "/monsters/static/js/runtime~main.e06e9654.js"
+  }
+]);
